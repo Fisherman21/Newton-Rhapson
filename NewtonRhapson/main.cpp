d569e92e1572610502d578f4ea3277d36e97db7e
Fisherman21 <johan.lundgren97@me.com>
2016-03-09 22:57:30 +0100

    Initial Commit

---
+++ NewtonRhapson/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NewtonRhapson
+//
+//  Created by Johan Lundgren on 2016-03-09.
+//  Copyright © 2016 SuperMilkApps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
